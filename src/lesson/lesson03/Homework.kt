0feb89d0f196d8bf1dda9df9fc3f44f54139d430
user <dssd@gmail.com>
2026-09-14 18:32:38 +0300

    commit0304

---
+++ src/lesson/lesson03/Homework.kt
@@ -0,0 +1,149 @@
+package lessons.lesson03.homework
+
+
+class Homework {
+}
+// 1. название мероприятия
+val eventName: String = "Hackathon Survival"
+
+// 2. дата проведения
+var eventDate: String = "20 сентября 2026"
+
+// 3. место проведения
+var eventLocation: String = "Москва"
+
+// 4. Подробный бюджет мероприятия
+private val detailedBudget: String = "Подробный бюджет"
+
+// 5. Количество участников
+var participantsCount: Int = 100
+
+// 6. Длительность хакатона
+val hackathonDuration: Int = 48
+
+// 7. Контактная информация и условия соглашений с поставщиками
+private var suppliersInfo: String = "Информация о поставщиках"
+
+// 8. Текущее состояние хакатона
+var hackathonStatus: String = "Подготовка"
+
+// 9. Список спонсоров
+var sponsors: String = "Список спонсоров"
+
+// 10. Бюджет мероприятия
+var eventBudget: Int = 1000000
+
+// 11. Текущий уровень доступа к интернету
+var internetAccess: Boolean = true
+
+// 12. Информация о транспортировке оборудования и распределении ресурсов
+private var logisticsInfo: String = "Информация о логистике"
+
+// 13. Количество команд
+var teamsCount: Int = 20
+
+// 14. Перечень задач
+var tasks: String = "Список задач"
+
+// 15. План эвакуации
+val evacuationPlan: String = "План эвакуации"
+
+// 16. Список доступного оборудования
+var equipment: String = "Список оборудования"
+
+// 17. Список свободного оборудования
+var freeEquipment: String = "Список свободного оборудования"
+
+// 18. График питания участников
+lateinit var mealSchedule: String
+
+// 19. План мероприятий на случай сбоев
+val failurePlan: String = "План действий при сбоях"
+
+// 20. Список экспертов и жюри
+var expertsAndJury: String = "Список экспертов и жюри"
+
+// 21. Политика конфиденциальности
+val privacyPolicy: String = "Политика конфиденциальности"
+
+// 22. Приватные отзывы участников и зрителей
+private var feedback: String = "Отзывы"
+
+// 23. Текущая температура в помещении
+var roomTemperature: Int = 22
+
+// 24. Данные мониторинга сети
+var networkMonitoring: String = "Данные мониторинга"
+
+// 25. Регулятор скорости интернета
+var internetSpeedRegulator: String = "0.5"
+
+// 26. Уровень освещения
+var lightingLevel: Int = 70
+
+// 27. Лог событий мероприятия
+private var eventLog: String = "Лог событий"
+
+// 28. Возможность получения медицинской помощи
+var medicalHelpAvailable: Boolean = true
+
+// 29. Планы и процедуры обеспечения безопасности
+private val securityPlan: String = "План безопасности"
+
+// 30. Регистрационный номер мероприятия
+val registrationNumber: String = "001"
+
+// 31. Максимально допустимый уровень шума
+val maximumNoiseLevel: Int = 80
+
+// 32. Индикатор превышения уровня шума
+var noiseLimitExceeded: Boolean = false
+
+// 33. План взаимодействия с прессой
+private var pressPlan: String = "План взаимодействия с прессой"
+
+// 34. Детальная публичная информация о проектах каждой команды
+val teamProjectsInfo: String by lazy {
+    "Информация о проектах команд"
+}
+
+// 35. Статус получения всех необходимых разрешений
+var permissionsReceived: Boolean = false
+
+// 36. Индикатор доступности специализированного оборудования
+var specialEquipmentAvailable: Boolean = true
+
+// 37. Список партнеров мероприятия
+var partners: String = "Список партнеров"
+
+// 38. Отчет с фотографиями, видео и отзывами
+val finalReport: String by lazy {
+    "Итоговый отчет"
+}
+
+// 39. Внутренние правила распределения призов
+private val prizeRules: String = "Правила распределения призов"
+
+// 40. Список разыгрываемых призов
+val prizes: String = "Список призов"
+
+// 41. Контактная информация экстренных служб
+private val emergencyContacts: String = "Контакты экстренных служб"
+
+// 42. Особые условия для участников с ограниченными возможностями
+var accessibilityConditions: String = "Особые условия"
+
+// 43. Общее настроение участников
+var participantsMood: String = "Хорошее"
+
+// 44. Подробный план хакатона
+lateinit var hackathonPlan: String
+
+// 45. Имя знаменитого специального гостя
+lateinit var specialGuestName: String
+
+// 46. Максимальное количество людей
+val maximumCapacity: Int = 500
+
+// 47. Количество часов для работы каждой команды
+val hoursPerTeam: Int = 48
