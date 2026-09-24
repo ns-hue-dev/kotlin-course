e91023cd40f0b5fe7e3250d42899f66b7bdc0366
user <dssd@gmail.com>
2026-09-24 18:18:30 +0300

    commit07_2

--- src/main/kotlin/lesson/lesson07/Homework07.kt
+++ src/main/kotlin/lesson/lesson07/Homework07.kt
@@ -1,5 +1,3 @@
-
-
 fun main() {
 //Задания для цикла for
 //Task 1
@@ -14,14 +12,14 @@ fun main() {
             println(j)
         }
     }
- //Task 3
+    //Task 3
     println("Task 3")
     for (i in 1..5) {
         println(6 - i)
     }
- //Task 4
+    //Task 4
     println("Task 4")
-    for(i in 10 downTo 1 step 2) {
+    for (i in 10 downTo 1 step 2) {
         println(i)
     }
 //Task 5
@@ -40,13 +38,13 @@ fun main() {
     for (i in 3..size) {
         println(i)
     }
- println("Task 8")
-var count = 0
+    println("Task 8")
+    var count = 0
     while (++count <= 5) {
-        println(count*count)
+        println(count * count)
     }
-println("Task 9")
-    count =5
+    println("Task 9")
+    count = 5
     while (++count < 10) {
         println(count)
     }
@@ -62,40 +60,100 @@ println("Task 9")
         println(count)
     } while (++count < 10)
     println("Task 12")
-    for (i in  1..10 ) {
+    for (i in 1..10) {
         println(i)
-        if (i ==6) break
+        if (i == 6) break
     }
-println("Task 13")
- var countmax=999
-    count =0
+    println("Task 13")
+    var countmax = 999
+    count = 0
     while (++count < countmax) {
         println(count)
-        if (count ==10) break
+        if (count == 10) break
     }
     println("Task 14")
-    for(i in 1..10) {
-        if (i%2 == 0) continue
+    for (i in 1..10) {
+        if (i % 2 == 0) continue
         else {
             println(i)
         }
 
     }
-println("Task 15")
+    println("Task 15")
     count = 0
     while (++count < 10) {
-        if (count%3 == 0) continue
+        if (count % 3 == 0) continue
         else {
             println(count)
         }
     }
 
 
+    println("Task 1.1")
+    for (i in 1..10) {
+        for (j in 1..10) {
+            print("${j * i} ")
+        }
+        println()
+    }
+    println("Task 1.2")
+    var arg = 3
+    count = 0
+    for (i in 1..arg) {
+        count = count + i
+    }
+    println(count)
 
 
 
+    println("Task 1.3")
+    var argF = 1
+    arg = 4
+    count = 0
+    while (++count <= arg) {
+        argF = argF * count
+    }
+    println("Factorial $arg = $argF")
+
+    println("Task 1.4")
+    arg = 6
+    count = 0
+    var argsum = 0
+    while (++count <= arg) {
+        if (count % 2 == 1) continue
+        else {
+            argsum = argsum + count
+        }
+    }
+    println("Сумма четных чисел до $arg = $argsum")
+
+    println("Task 1.5")
+    count = 0
+    var count2 = 0
+    while (++count <= 3) {
+        println("-----------")
+        while (++count2 <= 5) {
+            print("|*")
+        }
+        println("|")
+        count2 = 0
+    }
+    println("-----------")
 
 
+    println("Task 1.6")
+    arg = 5
+    var sumEven = 0
+    var sumOdd = 0
+    for (i in 1..arg) {
+        if (i % 2 == 1) {
+            sumOdd += i
+        }
+        if (i % 2 == 0) {
+            sumEven += i
+        }
+    }
+    println("Сумма четных $sumEven сумма нечетных $sumOdd")
 
 }
 
